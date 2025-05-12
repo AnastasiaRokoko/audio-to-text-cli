1741ddd72dec0caee78f2e4a43242204a89f9b0f
AnastasiaRokoko <an.troyckina@yandex.ru>
2025-05-12 16:02:19 +0300

    Add updates to server logic

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,58 +1,18 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
-import Fastify from "fastify";
-import multipart from "@fastify/multipart";
-import path from "path";
 import fs from "fs";
-import { transcribeAudio } from "../src/whisper";
-import { pipeline } from "stream/promises";
+import { createServer } from "../src/server";
 import fetch from "node-fetch";
 import FormData from "form-data";
 
-// Сервер
-function buildServer() {
-  const server = Fastify();
-  server.register(multipart);
-
-  const tempDir = path.resolve("temp");
-  if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
-
-  server.post("/audio-to-text", async (req, reply) => {
-    const parts = await req.parts();
-
-    for await (const part of parts) {
-      if (part.type === "file" && part.fieldname === "audio") {
-        const tempFilePath = path.join(tempDir, part.filename);
-        await pipeline(part.file, fs.createWriteStream(tempFilePath));
-
-        try {
-          const text = await transcribeAudio(tempFilePath);
-          return { text };
-        } catch (err) {
-          reply.status(500);
-          return {
-            error: "Transcription failed",
-            details: err instanceof Error ? err.message : String(err),
-          };
-        } finally {
-          fs.unlinkSync(tempFilePath);
-        }
-      }
-    }
-
-    reply.status(400).send({ error: "No audio file found in 'audio' field." });
-  });
-
-  return server;
-}
-
 describe("POST /audio-to-text", () => {
-  let server: ReturnType<typeof buildServer>;
+  //обращение к созданному серверу
+  let server: ReturnType<typeof createServer>;
   let address: string;
 
   //перед тестами запуск сервера и запоминание его адреса
   beforeAll(async () => {
-    server = buildServer();
-    await server.listen({ port: 0 }); // запуск на случайном порту
+    server = createServer();
+    await server.listen({ port: 0 }); // запуск на свободном порту
     address = `http://localhost:${(server.server.address() as any).port}`;
   });
   //остановка сервера после тестов

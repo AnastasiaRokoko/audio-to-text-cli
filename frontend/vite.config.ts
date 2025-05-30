73f64486af61a72730ab077ea5f06321322c2b5d
AnastasiaRokoko <an.troyckina@yandex.ru>
2025-05-30 11:00:49 +0300

    changes in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({}); 
+import { defineConfig } from "vite";
+export default defineConfig({
+  optimizeDeps: {
+    include: ["@ics/gx-vector-search"],
+  },
+});
